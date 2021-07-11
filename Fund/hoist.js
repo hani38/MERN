0891c10a846bb6b6fbe711b84c114c5f3175234d
hani38 <83581846+hani38@users.noreply.github.com>
2021-07-11 04:15:41 -0700

    Hoist Assignment

---
+++ Fund/hoist.js
@@ -0,0 +1,183 @@
+// console.log(hello);                                   
+// var hello = 'world';                                 
+
+// var hello;
+// console.log(hello);
+// hello = 'world';
+
+// undifaind 
+
+
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+
+
+// var needle;
+// needle = 'haystack';
+// function test(){
+//         var needle = 'magnet';
+//         console.log(needle);
+//     }
+// test();
+
+// output: magnet
+
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+
+// var brendan ;
+// brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+// output: super cool . 
+
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+
+
+// var food ;
+// function eat(){
+//         food = 'half-chicken';
+//         console.log(food);
+//         var food = 'gone';
+//     }
+// food = 'chicken';
+// console.log(food);
+// eat();
+
+//output :chicken , half-chicken.
+
+
+// mean();
+// console.log(food);
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+// var mean;
+// mean();
+// console.log(food);
+// mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+//output : error .
+
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+
+
+// var genre ;
+
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+
+// console.log(genre); ///undefined
+// genre = "disco";
+// rewind();
+// console.log(genre);
+
+///output : undefined , rock , r&b , disco .
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+
+// var dojo;
+// function learn() {
+    
+//         dojo = "seattle";
+//         console.log(dojo);
+//         dojo = "burbank";
+//         console.log(dojo);
+//     }
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// console.log(dojo);
+
+//output  , san jose  , seattle , burbank , san jose.
+
+
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
+// function makeDojo(name, students){
+//     const dojo = {};
+//     dojo.name = name;
+//     dojo.students = students;
+//     if(dojo.students > 50){
+//         dojo.hiring = true;
+//     }
+//     else if(dojo.students <= 0){
+//         dojo = "closed for now";
+//     }
+//     return dojo;
+// }
+
+
+
+// function makeDojo(name, students){
+//         const dojo = {};
+//     dojo.name = name;
+//     dojo.students = students;
+//     if(dojo.students > 50){
+//         dojo.hiring = true;
+//     }
+//     else if(dojo.students <= 0){
+//         dojo = "closed for now";
+//     }
+//     return dojo;
+// }
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
+ 
+//output : first : {name: 'Chicago', students: 65, hiring: true}.
+//output : second : error. 
+          

c3b311302b18be29a27314608e375e7f16abb1ea
hani38 <hanikiwan9@gmail.com>
2021-07-14 06:43:52 -0700

    Big inversion

---
+++ React/big_inversion/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import PersonCard from './PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+
+     <PersonCard firstName="John" lastName="Smith" age={ 8 } hairColor="Brown" /> 
+     <PersonCard firstName="abiheran" lastName="motaza" age={ 10 } hairColor="Black" /> 
+     <PersonCard firstName="hani" lastName="kiwan" age={ 20 } hairColor="White" /> 
+
+
+    </div>
+  );
+}
+
+export default App;

cccd264b2ef5ce2e5065add59de23ee36347dedc
hani38 <hanikiwan9@gmail.com>
2021-07-15 07:24:15 -0700

    hook form validation

--- React/hook_form_validation/src/UserForm.jsx
+++ React/hook_form_validation/src/UserForm.jsx
@@ -20,6 +20,33 @@ const UserForm = () => {
         setConfirmpassword("")
     };
 
+    const validationfirstname = (e)  =>{
+        
+        if (firstname.length < 2)
+        return "it should be more than 2 ";
+    }
+    const validationlastname = (e)  =>{
+        if (lastname.length < 2)
+        return "it should be more than 2 ";
+    }
+    const validationemail = (e)  =>{
+        
+        if (email.length < 5)
+        return "it should be more than 5 ";
+    }
+    const validationpassword = (e)  =>{
+        
+        if (password.length < 8)
+        return "it should be more than 8 ";
+    }
+    const validationpasscon = (e)  =>{
+        
+        if (confirmpassword.length < 8)
+        return "it should be more than 8 ";
+    }
+
+
+
 
     return (
         <div>
@@ -27,25 +54,31 @@ const UserForm = () => {
             <form onSubmit={createUser}>
                 <div>
                     <label>FirstName: </label>
-                    <input type="text" onChange={(e) => setFirstname(e.target.value)} />
+                    <input type="text" onChange=  { (e) => setFirstname(e.target.value)} />
+                    <p> {validationfirstname()}</p>
+                   
                 </div>
 
                 <div>
                     <label>LastName: </label>
-                    <input type="text" onChange={(e) => setLastname(e.target.value)} />
+                    <input type="text" onChange={ (e) => setLastname(e.target.value)} />
+                    <p> {validationlastname()}</p>
                 </div>
                 <div>
                     <label>Email Address: </label>
-                    <input type="text" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="text" onChange={ (e) => setEmail(e.target.value)} />
+                    <p> {validationemail()}</p>
                 </div>
                 <div>
                     <label>Password: </label>
-                    <input type="password" onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" onChange={ (e) => setPassword(e.target.value)} />
+                    <p> {validationpassword()}</p>
                 </div>
 
                 <div>
                     <label>Confirm Password: </label>
-                    <input type="password" onChange={(e) => setConfirmpassword(e.target.value)} />
+                    <input type="password" onChange={ (e) => setConfirmpassword(e.target.value)} />
+                    <p> {validationpasscon()}</p>
                 </div>
                 <input type="submit" value="Create User" />
             </form>
